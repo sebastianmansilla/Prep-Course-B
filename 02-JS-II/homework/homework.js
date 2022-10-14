bf0a23ac138412f6732f42d255edb4db9f24c8e0
seba10 <sebastianmansilla222@gmail.com>
2022-10-14 11:54:23 -0300

    tarea

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -145,18 +145,26 @@ function devolverString(str) {
     //Si el número es negativo, devolver ---> "Es negativo"
     //Si el número es 0, devuelve false
     // Tu código:
+    if (numero > 0){
+      return "Es positivo"
+    }else if (numero < 0){
+      return "Es negativo"
+    }
+    return false;
   }
   
   function agregarSimboloExclamacion(str) {
     // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
     // Ejemplo: "hello world" pasaría a ser "hello world!"
     // Tu código:
+    return str + "!";
   }
   
   function combinarNombres(nombre, apellido) {
     // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
     // Ejemplo: "Soy", "Henry" -> "Soy Henry"
     // Tu código:
+    return nombre + " " + apellido;
   }
   
   function obtenerSaludo(nombre) {
@@ -169,22 +177,26 @@ function devolverString(str) {
   function obtenerAreaRectangulo(alto, ancho) {
     // Retornar el area de un rectángulo teniendo su altura y ancho
     // Tu código:
+    return alto * ancho;
   }
   
   function retornarPerimetro(lado) {
     //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
     //Escribe tu código aquí
+    return lado * 4;
   }
   
   function areaDelTriangulo(base, altura) {
     //Desarrolle una función que calcule el área de un triángulo.
     //Escribe tu código aquí
+    return base * altura / 2;
   }
   
   function deEuroAdolar(euro) {
     //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
     //como parámetro un número de euros y calcule el cambio en dólares.
     //Escribe tu código aquí
+    return euro * 1.20;
   }
   
   // No modificar nada debajo de esta línea
